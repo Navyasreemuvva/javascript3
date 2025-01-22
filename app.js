fe967fad169bdae6cec6a375ed33372c73f8274c
navya1234567890 <168154729+navya1234567890@users.noreply.github.com>
2025-01-22 12:47:15 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //asynchronus function
 //making a waay for other functions to execute 1st
 
-const { object } = require("webidl-conversions");
+//const { object } = require("webidl-conversions");
 
 //two methods
 //1) setTImeOut(call back function, delay-time)
@@ -30,16 +30,44 @@ const { object } = require("webidl-conversions");
 //1) pending phase
 //2) reject phase
 //3)
-let online = window.Navigator.online
+// let online = window.Navigator.online
 
-new Promise((resolve, reject) => {
-    setTimeout(() => {
-        if(online === true){
+// new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         if(online === true){
+//             return resolve()
+//         }else{
+//             return reject()
+//         }
+//     } , 2000)
+// })
+// .then(() => console.log("online")
+// )
+
+function main(m,n){
+    new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if(isNaN(m) || isNaN(n)){
+                return reject()
+            }
+            for(let i=m;i<=n;i++){
+                console.log(i);
+            }
             return resolve()
-        }else{
-            return reject()
-        }
-    } , 2000)
-})
-.then(() => console.log("online")
-)
+        },3000)
+    })
+    .then(() => console.log("Promise is resolve"))
+    .then(() => console.log(10 * 10))
+    .catch(() => console.log("error occur"))
+}
+main(1, 10)
+ 
+
+
+
+// Promise training
+// creating multiple then methods is promise training
+
+
+// async and await keyword
+// this is to avoid promise training which avoids multiple then methods
