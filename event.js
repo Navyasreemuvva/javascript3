5a753ee342d3f1f98689054c71e05e076be1404e
navya1234567890 <168154729+navya1234567890@users.noreply.github.com>
2025-01-27 12:18:50 +0530

    Add files via upload

--- event.js
+++ event.js
@@ -22,6 +22,42 @@
         //click-event
 
 
-function event1(){
-    console.log("hello-world")
-}
+ function event1(){
+     console.log("hello-world")
+ }
+
+// let btn = document.getElementById("demo")
+// console.log(btn);
+
+// btn.onclick = function(){
+    // let res = Math.random() * 1000
+    // console.log(res);
+
+//     let res = Math.round(Math.random() * 10000).toString(16)
+//     console.log(res);
+//     document.body.style.backgroundColor = `#${res}`
+// }
+
+// let btn1 = document.getElementById("demo1")
+// console.log(btn1);
+// let color = window.prompt("enter any color name")
+// console.log(color);
+
+// btn.innerHTML = color
+
+// btn1.onmouseover = () => {
+//     document.body.style.backgroundColor = `${color}`
+
+// }
+
+let video = document.getElementById("demo2")
+console.log(video);
+
+video.addEventListener("click", () = > {
+    let res = video.classList.toggle("demo")
+    if(res){
+        video.play()
+    }else{
+        video.pause()
+    }
+})
